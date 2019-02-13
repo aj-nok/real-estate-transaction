4222ed314ec7d3618f785bcc3771f13a46768dbd
aj-nok <47238495+aj-nok@users.noreply.github.com>
2019-02-13 14:07:35 +0530

    Create Linear_Regression.py

---
+++ Linear_Regression.py
@@ -0,0 +1,37 @@
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn import cross_validation
+import pandas as pd
+import numpy as np 
+
+model = LinearRegression()
+df=pd.read_csv("realestatetransactions_new.csv")
+
+
+X=df['sq__ft']
+Y=df['price']
+X=X.values.reshape(len(X),1)
+Y=Y.values.reshape(len(Y),1)
+
+
+X_train = X[:-250]                        # Split the targets into training/testing sets Manually
+X_test = X[-250:]
+Y_train = Y[:-250]
+Y_test = Y[-250:]
+ 
+
+plt.scatter(X_test, Y_test,  color='black')
+plt.title('Test Data')
+plt.xlabel('Size')
+plt.ylabel('Price')
+plt.xticks(())
+plt.yticks(())
+
+
+regr =LinearRegression()
+regr.fit(X_train, Y_train)
+plt.plot(X_test, regr.predict(X_test), color='red',linewidth=3)
+plt.show()
+print(regr.predict(X_test))
